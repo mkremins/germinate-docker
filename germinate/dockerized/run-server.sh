821b72554e9a81867a5a1f2145a69d760e3d57c1
Max Kreminski <maxkreminski@gmail.com>
2022-10-24 11:26:39 -0700

    Remove leading slashes in paths in `run-server.sh`
    For some reason, absolute paths don't seem to behave correctly on Barrett's machine.

--- germinate/dockerized/run-server.sh
+++ germinate/dockerized/run-server.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
 echo Using Node.js $(node --version)
-
-cd /Germinate && python3 -m http.server 9000 &
-
-cd /Gemini/server && node index.js
+cd Germinate && python3 -m http.server 9000 &
+cd Gemini/server && node index.js
